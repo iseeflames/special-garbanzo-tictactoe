64405ba329c35364ddb642932e350eda167cfec9
scarface <pphiri1012x@gmail.com>
2022-02-03 13:53:23 +0200

    Create board and populate with squares.

--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -2,14 +2,16 @@ import React from "react";
 import Square from "./Square";
 import "./Board.css";
 
-export default function Board() {
+export default function Board({ playerMovePositions }) {
+  //console.log(playerMovePositions);
+
   return (
     <div>
+      <h2>Current player is {}</h2>
       <div className="board">
-        <Square playerMark="X" />
-        <Square playerMark="X" />
-        <Square playerMark="O" />
-        <Square />
+        {playerMovePositions.map((square, index) => {
+          return <Square value={index} key={index} />;
+        })}
       </div>
     </div>
   );
--- tictactoe/src/components/Square.js
+++ tictactoe/src/components/Square.js
@@ -1,21 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Square.css";
 
-export default function Square({ playerMark }) {
-  if (playerMark === "X") {
-    return (
-      <div>
-        <button className="square">{playerMark}</button>
-      </div>
-    );
-  } else if (playerMark === "O") {
-    return (
-      <div>
-        <button className="square">{playerMark}</button>
-      </div>
-    );
-  }
-
+export default function Square() {
   return (
     <div>
       <button className="square"></button>
